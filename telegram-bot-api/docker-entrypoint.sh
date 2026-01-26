4720e34e12d18e56f9944bd00e53932d5b739c9c
hanwg <han.wuguang@gmail.com>
2026-01-26 20:36:39 +0800

    test

--- telegram-bot-api/docker-entrypoint.sh
+++ telegram-bot-api/docker-entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 set -e
 
-echo 123
-pwd
-ls /data
-cat /data/options
-
 # load home assistant add-on configs
 if [ -f "/data/options.json" ]; then
   API_ID=$(jq --raw-output ".api_id" /data/options.json)
   API_HASH=$(jq --raw-output ".api_hash" /data/options.json)
 fi
+
+mkdir -p /data/telegram-bot-api
 
 /usr/local/bin/telegram-bot-api \
   --local \
