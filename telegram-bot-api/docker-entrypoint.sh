8c9c12772e12a71cab856e355dfb1964c0b8cfb5
hanwg <han.wuguang@gmail.com>
2026-01-29 15:54:41 +0000

    image

--- telegram-bot-api/docker-entrypoint.sh
+++ telegram-bot-api/docker-entrypoint.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
-cat /data/options.json
-echo 111
 API_ID="$(bashio::config 'api_id')"
 API_HASH="$(bashio::config 'api_hash')"
 LOG_LEVEL="$(bashio::config 'log_level')"
-echo "target is ---- $LOG_LEVEL"
-
-
-# load home assistant app configs
-if [ -f "/data/options.json" ]; then
-  API_ID=$(jq --raw-output ".api_id" /data/options.json)
-  API_HASH=$(jq --raw-output ".api_hash" /data/options.json)
-  LOG_LEVEL=$(jq --raw-output ".log_level" /data/options.json)
-fi
 
 mkdir -p /data/telegram-bot-api
 
